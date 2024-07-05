684c4898f187159c55cbb70bef5120efc8ec139a
Jayson Mercurio <jaysonpmercurio@gmail.com>
2024-07-05 16:19:36 -0700

    changed bootloader settings

--- keyboards/lily58/keymaps/jaysonmercurio/config.h
+++ keyboards/lily58/keymaps/jaysonmercurio/config.h
@@ -26,6 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 100
 
